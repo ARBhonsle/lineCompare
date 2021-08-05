2c512384f11d100e6cced743e4385f2abdfa4912
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-05 10:28:43 +0530

    Added: LineCompare Project files

---
+++ src/main/java/org/example/LineCompare.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ *
+ *
+ */
+public class LineCompare
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Line Comparison Computation Program!" );
+    }
+}
